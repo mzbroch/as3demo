f8318925beefa4d15b5d9a6bbae1ba817cc9a018
mzb <marek@zbroch.com>
2018-11-19 14:43:02 +0100

    Test

---
+++ send_declaration.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import json
+import os
+import sys
+
+import yaml
+
+
+class ApiWrapper(object):
+    def __init__(self, api_url=None, api_username=None, api_password=None):
+        if not api_url or not api_username or not api_password:
+            raise ValueError("Please provide API url, username and password")
+        else:
+            self.api_url = api_url
+            self.api_username = api_username
+            self.api_password = api_password
+
+    def post(self, endpoint, payload):
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.post('{}{}'.format(self.api_url, endpoint),
+                                 auth=(self.api_username, self.api_password),
+                                 headers=headers, data=payload, verify=False)
+
+        response.raise_for_status()
+
+        return response
+
+
+class F5Handler(object):
+    def __init__(self, f5_address=None, api_username=None, api_password=None):
+        if not f5_address or not api_username or not api_password:
+            raise ValueError(
+                "Please provide API address, username and password")
+        else:
+            self.api_wrapper = ApiWrapper(api_url=f5_address,
+                                          api_username=api_username,
+                                          api_password=api_password)
+
+    def send_declaration(self, payload):
+        endpoint = 'mgmt/shared/appsvcs/declare'
+
+        self.api_wrapper.post(endpoint, json.dumps(payload))
+
+
+def main():
+    f5_device = F5Handler(
+        f5_address='https://{}:8443/'.format(os.environ.get('IPADDR')),
+        api_username='admin',
+        api_password=os.environ.get('PASSWORD'))
+
+    with open('declaration.yml') as yml:
+        declaration_data = yaml.load(yml)
+
+    f5_device.send_declaration(declaration_data)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
